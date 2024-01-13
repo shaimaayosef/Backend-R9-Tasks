b0006ff3a6d9818e5627f684d44f0f3d4fafafa3
shimaa sayed <shimaa.sayed8888@gmail.com>
2024-01-13 22:12:22 +0200

    task2 13-1-2024

---
+++ task - 2 - php variables/myFirstPHPCode.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" >
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>
+       Document
+    </title>
+</head>
+<body>
+        <?php
+        $name1 ="shaimaa";
+        $age = "36";
+        $favorateBook = "thinking possitive";
+        echo "hello my name is $name1 and I'm $age years old, my favorate book is $favorateBook ";
+        ?>
+</body>
+</html>
