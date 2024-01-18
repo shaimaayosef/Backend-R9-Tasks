7f6a0f34c4b027244701d409ec248d488b27f95f
shimaa sayed <shimaa.sayed8888@gmail.com>
2024-01-18 21:29:58 +0200

    task3

---
+++ task-3-php-manublate-string/task3.php
@@ -0,0 +1,13 @@
+<?php
+$string1 = "Good Morning.";
+$string2 = "Have a nice day!";
+echo $string1 ." ". $string2 ."<br>"; 
+
+$string3 = "hello my name is shaimaa";
+echo strtoupper($string3) ."<br>";
+echo ucfirst($string3) ."<br>";
+echo ucwords($string3) ."<br>";
+
+$string4 = "I AM AN ACCOUNTANT";
+echo strtolower($string4) ."<br>";
+?>
